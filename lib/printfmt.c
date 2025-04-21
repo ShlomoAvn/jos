3ebb5a6541693c077ae4f8d16c130d0d93b5f288
ShlomoAvn <shlomo.avnon@campus.technion.ac.il>
2025-04-21 21:30:20 +0300

    my solution to lab1

--- lib/printfmt.c
+++ lib/printfmt.c
@@ -206,7 +206,7 @@ vprintfmt(void (*putch)(int, void*), void *putdat, const char *fmt, va_list ap)
 		// (unsigned) octal
 		case 'o':
 			num = getuint(&ap, lflag);
-			base = 16;
+			base = 8;
 		    goto number;
 
 
